97e5375060c4e8d6652be118e676f41e40fa97b3
liubida <dada.chao.liu@gmail.com>
2018-03-15 15:23:06 +0800

    fix cache.py

--- cache.py
+++ cache.py
@@ -18,21 +18,6 @@ class Cache:
             ex4: { price: [bid1, ask1], timestamp: '' }
         },
     }
-    p1: {
-        ex1:
-        ex2:
-    },
-    find(p, ex)
-    1 p ex exist
-    2 p ex youxiao
-
-    get(p, ex)
-    return 
-
-    set(p, ex, value)
-    a[p][ex][price] = value
-    a[p][ex][timestemp] = timestamp 
-
     '''
 
     def __init__(self):
@@ -40,15 +25,13 @@ class Cache:
         self.update_timeout = 10 # sec
         self.clean_timeout = 3600 * 24 # one day
 
-
     def find(self, symbol, exchange):
         if symbol not in self.data.keys():
             return False
         if exchange not in self.data[symbol].keys():
             return False
-        ts = self.data[symbol][exchange]
-        now = time.time()
-        if now - ts <=  self.datacache.update_timeout:
+        ts = self.data[symbol][exchange]['timestamp']
+        if time.time() - ts < self.datacache.update_timeout:
             return True
         return False            
     
@@ -57,15 +40,12 @@ class Cache:
             return self.data[symbol][exchange]['price']
         return None         
 
-
     def setvalue(self, symbol, exchange, value):
         if symbol not in self.data:
             self.data[symbol] = {}
         if exchange not in self.data[symbol]:
             self.data[symbol][exchange] = {}
-        self.data[symbol][exchange]['price'] = value
-        self.data[symbol][exchange]['timestamp'] = time.time()
-
+        self.data[symbol][exchange] = {'price': value, 'timestamp': time.time() }
 
 if __name__ == '__main__':
     pass
